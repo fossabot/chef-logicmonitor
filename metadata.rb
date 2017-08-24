09f841a2c0f6a3d90e58e345b95493f2f02ee64a
Matt Kasa <mattk@granicus.com>
2017-08-24 11:56:59 -0700

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mattk@granicus.com'
 license 'apachev2'
 description 'Installs/Configures logicmonitor'
 long_description 'Installs/Configures logicmonitor'
-version '0.1.0'
+version '0.1.1'
 
 %w( aix amazon centos fedora freebsd debian oracle mac_os_x redhat suse opensuse opensuseleap ubuntu windows zlinux ).each do |os|
   supports os
